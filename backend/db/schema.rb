9a7cbbd7f5e98f21d5420c4d61b30011033862dd
asamigentoku <gentoku.asami.9v@stu.hosei.ac.jp>
2026-07-30 15:45:59 +0900

    SQliteの設定を追加した

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_06_11_125126) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-
   create_table "mail_logs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "mail_type"
